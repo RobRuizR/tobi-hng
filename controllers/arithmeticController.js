e92497e842bc163dcf62d58ce2cd8f9d7ec0fd97
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-11-05 05:01:59 +0100

    all possible intances handled

--- controllers/arithmeticController.js
+++ controllers/arithmeticController.js
@@ -1,34 +1,56 @@
 const handleArithmetic = async (req, res) => {
   const { x, y, operation_type } = req.body;
 
+  const operationType = operation_type.toLowerCase();
+
   if (!x || !y || !operation_type) {
     return res
       .status(400)
       .json({ message: "Kindly fill all required fields." });
   }
 
-  if (
-    operation_type !== "*" &&
-    operation_type !== "+" &&
-    operation_type !== "-"
-  ) {
-    return res.status(400).json({ message: "invalid operation type" });
-  }
-
   if (!Number.isInteger(x) || !Number.isInteger(y)) {
     return res.status(400).json({ message: "invalid data type" });
   }
 
+  const multiplicationIndex = operationType.indexOf("multip");
+  const productIndex = operationType.indexOf("product");
+  const additionIndex = operationType.indexOf("add");
+  const subtractionIndex = operationType.indexOf("subtract");
+  const removeIndex = operationType.indexOf("remove");
+
+  let operator;
+
+  if (multiplicationIndex !== -1) {
+    operator = "multiplication";
+  } else if (productIndex !== -1) {
+    operator = "multiplication";
+  } else if (additionIndex !== -1) {
+    operator = "addition";
+  } else if (subtractionIndex !== -1) {
+    operator = "subtraction";
+  } else if (removeIndex !== -1) {
+    operator = "subtraction";
+  } else {
+    return res.status(400).json({ message: "invalid operation type" });
+  }
+
   let result;
-  if (operation_type === "*") {
+  if (operator === "multiplication") {
     result = x * y;
-    return res.status(201).json({ slackUsername: "Intuneteq", result: result });
-  } else if (operation_type === "+") {
+    return res
+      .status(201)
+      .json({ slackUsername: "Intuneteq", operationType, result: result });
+  } else if (operator === "addition") {
     result = x + y;
-    return res.status(201).json({ slackUsername: "Intuneteq", result: result });
+    return res
+      .status(201)
+      .json({ slackUsername: "Intuneteq", operationType, result: result });
   } else {
     result = x - y;
-    return res.status(201).json({ slackUsername: "Intuneteq", result: result });
+    return res
+      .status(201)
+      .json({ slackUsername: "Intuneteq", operationType, result: result });
   }
 };
 
