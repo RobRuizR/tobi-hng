c245236dbf53009921fe64edda089e51ed07b008
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-11-05 05:11:20 +0100

    comments added for readability

--- controllers/arithmeticController.js
+++ controllers/arithmeticController.js
@@ -1,25 +1,25 @@
 const handleArithmetic = async (req, res) => {
   const { x, y, operation_type } = req.body;
 
-  const operationType = operation_type.toLowerCase();
+  const operationType = operation_type.toLowerCase(); //setting incoming string to lowercase
 
   if (!x || !y || !operation_type) {
     return res
       .status(400)
-      .json({ message: "Kindly fill all required fields." });
+      .json({ message: "Kindly fill all required fields." }); //if req body is not complete
   }
 
   if (!Number.isInteger(x) || !Number.isInteger(y)) {
-    return res.status(400).json({ message: "invalid data type" });
+    return res.status(400).json({ message: "invalid data type" }); //if x and y are not integers/number
   }
 
-  const multiplicationIndex = operationType.indexOf("multip");
-  const productIndex = operationType.indexOf("product");
-  const additionIndex = operationType.indexOf("add");
-  const subtractionIndex = operationType.indexOf("subtract");
-  const removeIndex = operationType.indexOf("remove");
+  const multiplicationIndex = operationType.indexOf("multip"); //checking for possible versions of multiplication in operation type
+  const productIndex = operationType.indexOf("product"); //checking for possible instance of product as request to handle multiplication
+  const additionIndex = operationType.indexOf("add"); //checking for possible instances to handle addition operation
+  const subtractionIndex = operationType.indexOf("subtract"); //checking for possible intances to handle subtraction operation
+  const removeIndex = operationType.indexOf("remove"); //checking for possible instances to handle subtraction operation
 
-  let operator;
+  let operator; //declaring the variable to handle whatever operation type sent in
 
   if (multiplicationIndex !== -1) {
     operator = "multiplication";
@@ -32,25 +32,38 @@ const handleArithmetic = async (req, res) => {
   } else if (removeIndex !== -1) {
     operator = "subtraction";
   } else {
-    return res.status(400).json({ message: "invalid operation type" });
+    return res.status(400).json({ message: "invalid operation type" }); //if no operation type is detected in operation_type body, its a bad request
   }
 
-  let result;
+  //we have our operation_type handling as operator
+  let result; //declaring a variable to hold result of computation
   if (operator === "multiplication") {
     result = x * y;
     return res
       .status(201)
-      .json({ slackUsername: "Intuneteq", operationType, result: result });
+      .json({
+        slackUsername: "Intuneteq",
+        operation_type: operator,
+        result: result,
+      });
   } else if (operator === "addition") {
     result = x + y;
     return res
       .status(201)
-      .json({ slackUsername: "Intuneteq", operationType, result: result });
+      .json({
+        slackUsername: "Intuneteq",
+        operation_type: operator,
+        result: result,
+      });
   } else {
     result = x - y;
     return res
       .status(201)
-      .json({ slackUsername: "Intuneteq", operationType, result: result });
+      .json({
+        slackUsername: "Intuneteq",
+        operation_type: operator,
+        result: result,
+      });
   }
 };
 
