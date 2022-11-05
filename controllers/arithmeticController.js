e051c038ebe1620f8115e6dc9abaa227cd954863
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-11-05 05:26:59 +0100

    accounting for more instances

--- controllers/arithmeticController.js
+++ controllers/arithmeticController.js
@@ -1,4 +1,4 @@
-const handleArithmetic = async (req, res) => {
+const handleArithmetic = (req, res) => {
   const { x, y, operation_type } = req.body;
 
   const operationType = operation_type.toLowerCase(); //setting incoming string to lowercase
@@ -16,8 +16,10 @@ const handleArithmetic = async (req, res) => {
   const multiplicationIndex = operationType.indexOf("multip"); //checking for possible versions of multiplication in operation type
   const productIndex = operationType.indexOf("product"); //checking for possible instance of product as request to handle multiplication
   const additionIndex = operationType.indexOf("add"); //checking for possible instances to handle addition operation
+  const sumIndex = operationType.indexOf("sum"); //checking for possible instances to handle addition operation
   const subtractionIndex = operationType.indexOf("subtract"); //checking for possible intances to handle subtraction operation
   const removeIndex = operationType.indexOf("remove"); //checking for possible instances to handle subtraction operation
+  const takeAwayIndex = operationType.indexOf("take away"); //checking for possible instances to handle subtraction operation
 
   let operator; //declaring the variable to handle whatever operation type sent in
 
@@ -27,9 +29,13 @@ const handleArithmetic = async (req, res) => {
     operator = "multiplication";
   } else if (additionIndex !== -1) {
     operator = "addition";
+  } else if (sumIndex !== -1) {
+    operator = "addition";
   } else if (subtractionIndex !== -1) {
     operator = "subtraction";
   } else if (removeIndex !== -1) {
+    operator = "subtraction";
+  } else if (takeAwayIndex !== -1) {
     operator = "subtraction";
   } else {
     return res.status(400).json({ message: "invalid operation type" }); //if no operation type is detected in operation_type body, its a bad request
