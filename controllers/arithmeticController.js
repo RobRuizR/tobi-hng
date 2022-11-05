348a3c748e42816af4b5405da4beed54756310cb
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-11-05 09:54:16 +0100

    plot twist added

--- controllers/arithmeticController.js
+++ controllers/arithmeticController.js
@@ -1,17 +1,41 @@
 const handleArithmetic = (req, res) => {
   const { x, y, operation_type } = req.body;
 
-  const operationType = operation_type.toLowerCase(); //setting incoming string to lowercase
-
-  if (!x || !y || !operation_type) {
+  if (!operation_type) {
     return res
       .status(400)
       .json({ message: "Kindly fill all required fields." }); //if req body is not complete
   }
 
-  if (!Number.isInteger(x) || !Number.isInteger(y)) {
-    return res.status(400).json({ message: "invalid data type" }); //if x and y are not integers/number
+  const operationType = operation_type.toLowerCase(); //setting incoming string to lowercase
+
+  let intx; //instantiating the variable that will be used for the operation as x
+  let inty; //instantiating the variable that will be used for the operation as y
+
+  if (operationType.length > 14) {
+    //if operation_type is longer than 14 words, it's mostly likely telling us to operate the integers in it, if any at all
+    const myArray = operationType.split(" "); //declaring an array for eachwords in the operation_type string
+    const probableNum = myArray.filter((item) => {
+      if (parseInt(item) && typeof parseInt(item) === "number") {
+        //filter through the array and check for integers
+        return item;
+      }
+    });
+
+    if (probableNum.length < 2) {
+      //if we have less than 2 integers in the array, and we have x and y from the request, set the operating x and y to the req x and y
+      intx = x;
+      inty = y;
+    } else if ((probableNum.length < 2) & !x & !y) {
+      //if we have less than 2 integers in the array but we dont have x and y coming in from the request, respond with a bad request
+      return res.status(400).json({ message: "missing data types" });
+    } else {
+      intx = probableNum[0];
+      inty = probableNum[1];
+    }
   }
+
+  console.log(intx, inty, "values");
 
   const multiplicationIndex = operationType.indexOf("multip"); //checking for possible versions of multiplication in operation type
   const productIndex = operationType.indexOf("product"); //checking for possible instance of product as request to handle multiplication
@@ -20,8 +44,9 @@ const handleArithmetic = (req, res) => {
   const subtractionIndex = operationType.indexOf("subtract"); //checking for possible intances to handle subtraction operation
   const removeIndex = operationType.indexOf("remove"); //checking for possible instances to handle subtraction operation
   const takeAwayIndex = operationType.indexOf("take away"); //checking for possible instances to handle subtraction operation
+  const minusIndex = operationType.indexOf("minus"); //checking for possible instances to handle subtraction operation
 
-  let operator; //declaring the variable to handle whatever operation type sent in
+  let operator; //instantiating the variable to handle whatever operation type sent in
 
   if (multiplicationIndex !== -1) {
     operator = "multiplication";
@@ -37,6 +62,8 @@ const handleArithmetic = (req, res) => {
     operator = "subtraction";
   } else if (takeAwayIndex !== -1) {
     operator = "subtraction";
+  } else if (minusIndex !== -1) {
+    operator = "subtraction";
   } else {
     return res.status(400).json({ message: "invalid operation type" }); //if no operation type is detected in operation_type body, its a bad request
   }
@@ -44,32 +71,26 @@ const handleArithmetic = (req, res) => {
   //we have our operation_type handling as operator
   let result; //declaring a variable to hold result of computation
   if (operator === "multiplication") {
-    result = x * y;
-    return res
-      .status(201)
-      .json({
-        slackUsername: "Intuneteq",
-        operation_type: operator,
-        result: result,
-      });
+    result = intx * inty;
+    return res.status(201).json({
+      slackUsername: "Intuneteq",
+      operation_type: operator,
+      result: result,
+    });
   } else if (operator === "addition") {
-    result = x + y;
-    return res
-      .status(201)
-      .json({
-        slackUsername: "Intuneteq",
-        operation_type: operator,
-        result: result,
-      });
+    result = intx + inty;
+    return res.status(201).json({
+      slackUsername: "Intuneteq",
+      operation_type: operator,
+      result: result,
+    });
   } else {
-    result = x - y;
-    return res
-      .status(201)
-      .json({
-        slackUsername: "Intuneteq",
-        operation_type: operator,
-        result: result,
-      });
+    result = intx - inty;
+    return res.status(201).json({
+      slackUsername: "Intuneteq",
+      operation_type: operator,
+      result: result,
+    });
   }
 };
 
